93b3fdde1f56785e3f288b64840275bdf87f32e8
orvice <orvice@gmail.com>
2019-01-05 19:54:59 +0800

    add utils

---
+++ utils/utils.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/orvice/kit/log"
+)
+
+type Task struct {
+	name   string
+	t      time.Duration
+	fn     func() error
+	Logger log.Logger
+}
+
+type TaskOpt func(t *Task)
+
+func NewTask(name string, t time.Duration, fn func() error, opts ...TaskOpt) *Task {
+	task := &Task{
+		name:   name,
+		t:      t,
+		fn:     fn,
+		Logger: log.NewDefaultLogger(),
+	}
+	for _, opt := range opts {
+		opt(task)
+	}
+	return task
+}
+
+func (t *Task) Run() {
+	for {
+		start := time.Now()
+		err := t.fn()
+		t.Logger.Infof("run task %s cost: %v error: %v  ", t.name, err, time.Since(start))
+		time.Sleep(t.t)
+	}
+}
